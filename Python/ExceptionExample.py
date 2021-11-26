2ed7de8d84b88792a25ceeb6439dd24d8af035bd
Asen <asen__7@abv.bg>
2021-11-26 21:54:52 +0200

    \z

---
+++ Python/ExceptionExample.py
@@ -0,0 +1,10 @@
+import math
+
+try:
+    number = int(input('Insert a number: '))
+    
+    print(math.sqrt(number))
+except ValueError:
+    print('Invalid number')    
+finally:
+    print('Good Bye.')    
